493153ad2dc393c452e04be4e290458cf0416bd6
Gabriel Calebe <gabrielcalebe58@gmail.com>
2026-09-15 09:11:39 -0300

    Delete minimal-insert.sql

--- minimal-insert.sql
+++
@@ -1,9 +0,0 @@
--- Inserir evento mínimo - só campos básicos
-INSERT INTO events (id, title, description, location, date, vibe, createdby) 
-VALUES 
-  ('evt_1', 'Open Bar Eletrônico', 'Festa com DJs', 'Vila Madalena', 'weekend', 'Eletrônico', 'user_1');
-
--- Inserir usuário mínimo
-INSERT INTO users (id, email, name) 
-VALUES 
-  ('user_1', 'gabrielcalebe58@gmail.com', 'Gabriel');
