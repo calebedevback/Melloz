249bfcda18ef6bf6d418217ddeb2127587499b32
Gabriel Calebe <gabrielcalebe58@gmail.com>
2026-09-15 09:05:21 -0300

    Delete fix-rls-read.sql

--- fix-rls-read.sql
+++
@@ -1,7 +0,0 @@
--- Adicionar policy para leitura do próprio usuário
-CREATE POLICY "Users can read own profile" ON users FOR SELECT USING (
-  auth.uid()::text = id
-);
-
--- Verificar se a policy foi criada
-SELECT * FROM pg_policies WHERE tablename = 'users';
